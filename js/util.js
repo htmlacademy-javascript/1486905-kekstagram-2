f4189d72dcce04370f82e69f3f811186dde0dc0a
Ekaterina Rudakova <koshkina_e.p@mail.ru>
2024-12-16 00:27:53 +0300

    Правки перед защитой

--- js/util.js
+++ js/util.js
@@ -1,39 +1,13 @@
-import { DEBOUNCE_DELAY } from './constants.js';
+import { DEBOUNCE_DELAY, REMOVE_MESSAGE_TIMEOUT } from './constants.js';
 
 const errorLoadDataTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
 const body = document.body;
-const REMOVE_MESSAGE_TIMEOUT = 5000;
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
-
-const getRandomElement = (data) => data[getRandomInteger(0, data.length - 1)];
-
-const getUniqueRandom = (a, b) => {
-  const uniqueItems = [];
-  return () => {
-    if (uniqueItems.length > Math.abs(a - b)) {
-      return;
-    }
-    let unique = getRandomInteger(a, b);
-    while (uniqueItems.includes(unique)) {
-      unique = getRandomInteger(a, b);
-    }
-    uniqueItems.push(unique);
-    return unique;
-  };
-};
-
-const getCounter = () => {
-  let counter = 0;
-  return () => {
-    counter = counter + 1;
-    return counter;
-  };
 };
 
 export const showErrorMessage = (message) => {
@@ -58,6 +32,6 @@ function debounce(callback, timeoutDelay = DEBOUNCE_DELAY) {
   };
 }
 
-export { getRandomInteger, getRandomElement, getUniqueRandom, getCounter, debounce };
+export { getRandomInteger, debounce };
 
 
